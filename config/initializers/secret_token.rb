76656edc50abe36aa552396823f2ac23a0f36461
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-02-14 21:55:55 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restate::Application.config.secret_token = '8161cd08231824dbc8121b9f8ad57c2e6879489aa4ce580a3d930744d1d1338868f75897ba9d5e1f2d59583fd88aa67c58d1cf68042addfc74875adcd3064c8f'
